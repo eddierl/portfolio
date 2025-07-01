92e2dce5f8ebf5307001f23ce72a798b391fb81a
Eddie Erlich <eddie@rhino-eco.com>
2025-07-02 00:32:55 +0300

    fix

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -7,12 +7,17 @@ const BaseUrl = metaData.baseUrl.endsWith("/")
   : `${metaData.baseUrl}/`;
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  let blogs = getBlogPosts().map((post) => ({
+  const blogs = getBlogPosts().map((post) => ({
     url: `${BaseUrl}blog/${post.slug}`,
     lastModified: post.metadata.publishedAt,
   }));
 
-  let routes = ["", , "blog", "Eddie_Erlich_Resume.pdf"].map((route) => ({
+  const routes = [
+    "",
+    "blog",
+    "Eddie_Erlich_Resume.pdf",
+    //  "projects", "photos"
+  ].map((route) => ({
     url: `${BaseUrl}${route}`,
     lastModified: new Date().toISOString().split("T")[0],
   }));
